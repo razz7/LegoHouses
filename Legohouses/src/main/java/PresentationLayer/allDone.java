797d5f7ed867b8b5544a1af0ba352074fe7810a7
razz7 <rashem2@hotmail.com>
2019-03-25 00:54:24 +0100

    a

--- Legohouses/src/main/java/PresentationLayer/allDone.java
+++ Legohouses/src/main/java/PresentationLayer/allDone.java
@@ -5,11 +5,10 @@
  */
 package PresentationLayer;
 
+import FunctionLayer.Calculator;
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
 import FunctionLayer.OrderException;
-import FunctionLayer.OrderSampleException;
-
 import FunctionLayer.User;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,25 +22,24 @@ import javax.servlet.http.HttpServletResponse;
 public class allDone extends Command {
 
     @Override
-    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException, OrderSampleException, OrderException{
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-//        User user = LogicFacade.login(email, password);
-//        int width = Integer.parseInt(request.getParameter("width"));
-//        int length = Integer.parseInt(request.getParameter("length"));
-//        int height = Integer.parseInt(request.getParameter("height"));
-//
-//        if (width < 4 || length < 4 || height < 4) {
-//            try {
-//                throw new OrderSampleException("too small");
-//            } catch (OrderSampleException ex) {
-//                Logger.getLogger(allDone.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        } else {
-//            LogicFacade.createOrder(user, length, width, height, 0);
-//        }
-
+        User user = LogicFacade.login(email, password);
+        int length = Integer.parseInt(request.getParameter("length"));
+        int width = Integer.parseInt(request.getParameter("width"));
+        int height = Integer.parseInt(request.getParameter("height"));
+        int shipped = 0;
+        int order_id = 0;
+        Calculator cal = new Calculator();
+       
+        try {
+            LogicFacade.createOrder(user, order_id, length, width, height);
+        } catch (OrderException ex) {
+            Logger.getLogger(allDone.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
+
         return "allDone";
     }
 
